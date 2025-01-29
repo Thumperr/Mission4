5d3dc37ee98909fef77ee6de428817e0800d3554
Thumperr <asdf;lkj@>
2025-01-29 09:39:57 -0700

    deleted bad etufs

--- Board.cs
+++ Board.cs
@@ -1,10 +1,3 @@
-/*
-    • Receive the “board” array from the driver class
-    • Contain a method that prints the board based on the information passed to it
-    • Contain a method that receives the game board array as input and returns if there is a
-    winner and who it was
- */
-
 namespace Mission4;
 
 public class Board
