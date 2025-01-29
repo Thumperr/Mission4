0e615cb1496af8347d17cdb76abbcc0e404e0dd7
Thumperr <asdf;lkj@>
2025-01-29 08:07:34 -0700

    asdf;lkj

--- Board.cs
+++ Board.cs
@@ -1,10 +1,17 @@
+/*
+    • Receive the “board” array from the driver class
+    • Contain a method that prints the board based on the information passed to it
+    • Contain a method that receives the game board array as input and returns if there is a
+    winner and who it was
+ */
+
 namespace Mission4;
 
 public class Board
 {
-    public string doSomething()
+    public Array[] PrintBoard( /*Information*/)
     {
-        string output = "Doing Something!";
-        return output;
+        Array[] board = new Array[3];
+        Console.WriteLine(board);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿using Mission4;
+﻿/*
+• Welcome the user to the game
+• Create a game board array to store the players’ choices
+• Ask each player in turn for their choice and update the game board array
+• Print the board by calling the method in the supporting class
+• Check for a winner by calling the method in the supporting class, and notify the players
+when a win has occurred and which player won the game
+*/
+
+using Mission4;
 
 Board board = new Board();
 
